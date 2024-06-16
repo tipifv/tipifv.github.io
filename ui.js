3312de8c725e1c78d8f94d8b85ee4da2febe5b03
tipifv <tipifv@knu.ac.kr>
2024-06-17 02:07:47 +0900

    Add vanta

--- ui.js
+++ ui.js
@@ -101,3 +101,14 @@ const send = async function() {
 
 
 refreshChat();
+VANTA.BIRDS({
+	el: "#vantaContainer",
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	scale: 1.00,
+	scaleMobile: 1.00,
+	separation: 73.00
+})
